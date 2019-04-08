c91eee6cc57db0e3455147c7cb5f0691020bd56f
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 00:59:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
   def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU TOO PUMPKIN!"
-      puts "I LOVE YOU TOO PUMPKIN!
-      "I LOVE YOU TOO PUMPKIN!""
+      puts "I LOVE YOU TOO PUMPKIN!"
+      "I LOVE YOU TOO PUMPKIN!"
     elsif !phrase.upcase
       puts "HUH?! SPEAK UP, SONNY!"
       "HUH?! SPEAK UP, SONNY!"
