db760c9f997be0342469cc1023a77bd2985a1692
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 00:45:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@
     elsif phrase.upcase
       puts "NO, NOT SINCE 1938!"
       "NO, NOT SINCE 1938!"
-    else phrase == "I LOVE YOU GRANDMA"
+    else phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
       "I LOVE YOU TOO PUMPKIN!"
     end
