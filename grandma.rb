5162b2ce2c4e136a9272c6712ec35ce31400ef3a
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 00:14:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
   
-  def speak_to_grandma(phrase)
+  def speak_to_grandma
     if phrase.upcase==false
       puts "HUH?! SPEAK UP, SONNY!"
     end  
