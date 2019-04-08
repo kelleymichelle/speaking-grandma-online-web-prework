f8ed9e75e984b3ca689af0aef2dd8ac859e6a952
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 01:40:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
   def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase.upcase && true
+    elsif phrase.upcase
       "NO, NOT SINCE 1938!"
     else phrase == true
       "HUH?! SPEAK UP, SONNY!"
